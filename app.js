c9f469327bb179d56af2597abf4ddfd83d446bf1
Aissam Erradouane <erradouane.aissam96@gmail.com>
2018-10-23 17:09:40 +0100

    Adding Dockerfile and Docker-compose

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/loginapp');
+mongoose.connect('mongodb://172.20.0.2:27017/loginapp');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
